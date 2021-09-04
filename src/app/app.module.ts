f3da9fc053a546087742c86643127e6a6c1a0368
Tusharshrivastav <ts3657@gmail.com>
2021-09-04 15:51:43 +0530

    completed

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import {HttpClientModule}  from '@angular/common/http';
+import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component'
+import { GoogleChartsModule } from 'angular-google-charts';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent,
+    DashboardCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, 
+    HttpClientModule, 
+    GoogleChartsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
